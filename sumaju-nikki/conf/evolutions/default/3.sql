434d0e23f2031062ff1e365288cb287997254267
johnny-keker <giomail.iv@gmail.com>
2019-01-03 20:13:09 +0300

    fixed some issues in 3.sql and added stage representation in character table

--- sumaju-nikki/conf/evolutions/default/3.sql
+++ sumaju-nikki/conf/evolutions/default/3.sql
@@ -3,37 +3,39 @@
 # --- !Ups
 create table characters (
   id bigserial primary key,
-  name varchar
-);
+  name varchar,
+  current_stage varchar,
+  next_stage_time timestamp
+);;
 
 create table owls (
   id bigserial primary key,
   owl_type varchar
-);
+);;
 
 create table owls_characters (
   owl_id bigint not null references owls,
   character_id bigint not null references characters
-);
+);;
 
 create or replace function display_owls (username varchar)
 returns table (
   owl_type varchar,
   amount bigint
 )
-as $$
+as $body$
 begin
   return query
   select owls.owl_type, count(owls.owl_type) from owls, characters, owls_characters
   where owls.id = owls_characters.owl_id
   and characters.id = owls_characters.character_id
   and characters.name = username
-  group by owls.owl_type;
-end; $$
-language 'plpgsql';
+  group by owls.owl_type;;
+end;; $body$
+language 'plpgsql';;
 
 # --- !Downs
-drop table characters;
-drop table owls;
-drop table owls_characters;
-drop function display_owls;
+drop function display_owls;;
+drop table owls_characters;;
+drop table characters;;
+drop table owls;;
