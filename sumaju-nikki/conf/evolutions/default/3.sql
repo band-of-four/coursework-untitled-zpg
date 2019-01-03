b351ec4ccc5d7ea1a91d67e525c44af54570b5d5
johnny-keker <giomail.iv@gmail.com>
2019-01-04 01:25:09 +0300

    creature tables

--- sumaju-nikki/conf/evolutions/default/3.sql
+++ sumaju-nikki/conf/evolutions/default/3.sql
@@ -16,6 +16,29 @@ create table characters (
   attack_spell bigint not null references spells,
   defence_spell bigint not null references spells,
   luck_spell bigint not null references spells
+  pet_type bigint references creature_type,
+  pet_name varchar
+);;
+
+create table creature_type (
+  id bigserial primary key,
+  name varchar,
+  power int,
+  hp int,
+  level int -- for gating
+);;
+
+create table creature_phrases (
+  creature_type_id bigint not null references creature_type,
+  phrase_type varchar, -- enter the battle, winning the battle etc
+  phrase varchar
+);;
+
+-- personal creature battle modifier character can learn from a books
+create table creature_bonuses (
+  character_id bigint not null references characters,
+  creature_type_id bigint not null references creature_type,
+  modifier bigint
 );;
 
 create table owls_characters (
@@ -39,7 +62,10 @@ end;; $body$
 language 'plpgsql';;
 
 # --- !Downs
-drop function display_owls;;
-drop table spells;;
+drop function display_owls(username varchar);;
 drop table owls_characters;;
+drop table creature_bonuses;;
 drop table characters;;
+drop table spells;;
+drop table creature_phrases;;
+drop table creature_type;;
