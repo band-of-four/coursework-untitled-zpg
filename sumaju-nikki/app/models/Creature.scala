9a8e33ec04db04c67df73cf878564272cbe9678d
johnny-keker <giomail.iv@gmail.com>
2019-01-05 04:37:25 +0300

    creatures mapping

---
+++ sumaju-nikki/app/models/Creature.scala
@@ -0,0 +1,17 @@
+package models
+
+import db.DbCtx
+import scala.util.Random
+
+case class Creature(name: String, power: Int, hp: Int, level: Int)
+
+class CreatureDao(val db: DbCtx) {
+  import db._
+
+  private val schema = quote(querySchema[Creature]("creatures"))
+  
+  // Get creature for batte with character before room entering
+  def getRandomCreatureByLevel(level: Int): Creature =
+    Random.shuffle(run(schema.filter(_.level == lift(level)))).head
+
+}
