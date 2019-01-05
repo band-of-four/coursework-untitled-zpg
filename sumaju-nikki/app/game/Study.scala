29350872b452a53c3b8986d68063edb544f38690
timlathy <tim.labushev@gmail.com>
2019-01-05 22:27:08 +0700

    implement state transitions for travelling

---
+++ sumaju-nikki/app/game/Study.scala
@@ -0,0 +1,7 @@
+package game
+
+import java.time.Duration
+
+object Study {
+  val StudyDuration = Duration.ofSeconds(120)
+}
--- sumaju-nikki/app/services/GameProgressionService.scala
+++ sumaju-nikki/app/services/GameProgressionService.scala
@@ -2,8 +2,10 @@ package services
 
 import game.Travel
 import models.{LessonDao, RoomDao, Student, StudentDao}
-import models.Student.{StageTravel, StageFight, StageStudy}
+import models.Student.{StageFight, StageStudy, StageTravel}
 import game.Fight.FightChance
+import game.Travel.{TravelDuration, TravelRadius, AttendClass, ContinueTravelling, VisitClub}
+import game.Study.StudyDuration
 import utils.RandomEvent
 
 class GameProgressionService(val studentDao: StudentDao,
@@ -14,24 +16,29 @@ class GameProgressionService(val studentDao: StudentDao,
 
   def performUpdate(student: Student): Unit =
     student.stage match {
-      case StageTravel => finishTravelling(student)
+      case StageTravel =>
+        if (RandomEvent(FightChance)) startFighting(student)
+        else enterNextRoom(student)
       case StageFight => continueFighting(student)
       case StageStudy => finishStudying(student)
     }
-
-  def finishTravelling(student: Student): Unit = {
-    if (RandomEvent(FightChance))
-      startFighting(student)
-    else {
-      val nearbyRooms = roomDao.preloadInRadius(student.currentRoom, Travel.TravelRadius)
-      val attendance = lessonDao.buildAttendanceMap(student.id, nearbyRooms.flatMap(_.lesson.map(_.id)))
-      val nextRoom = Travel.pickNextRoom(student, nearbyRooms, attendance)
-    }
-  }
 
   def finishStudying(student: Student): Unit = ???
 
   def startFighting(student: Student): Unit = ???
 
   def continueFighting(student: Student): Unit = ???
+
+  def enterNextRoom(student: Student): Unit = {
+    val nearbyRooms = roomDao.preloadInRadius(student.currentRoom, TravelRadius)
+    val attendance = lessonDao.buildAttendanceMap(student.id, nearbyRooms.flatMap(_.lesson.map(_.id)))
+    Travel.pickDestination(student, nearbyRooms, attendance) match {
+      case AttendClass(newRoom) =>
+        studentDao.updateStage(student.id, newRoom, StageStudy, StudyDuration)
+      case VisitClub(newRoom) =>
+        ???
+      case ContinueTravelling(newRoom) =>
+        studentDao.updateStage(student.id, newRoom, StageTravel, TravelDuration)
+    }
+  }
 }
