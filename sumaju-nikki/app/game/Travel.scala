84f898dbc3e704e9fbc71423cc6170cd9d235c93
timlathy <tim.labushev@gmail.com>
2019-01-06 17:08:37 +0700

    use room level to pick travel direction

--- sumaju-nikki/app/game/Travel.scala
+++ sumaju-nikki/app/game/Travel.scala
@@ -20,14 +20,14 @@ object Travel {
 
   def pickDestination(student: Student, rooms: Seq[RoomPreloaded], attendance: LessonAttendanceMap): Destination = {
     val nextRoom = WeightedSample(rooms) {
-      case RoomPreloaded(_, Some(club), _) =>
-        0.15
-      case RoomPreloaded(_, _, Some(lesson)) if lesson.academicYear != student.academicYear =>
+      case RoomPreloaded(_, level, _, _) if level > student.academicYear =>
         0
-      case RoomPreloaded(_, _, Some(lesson)) =>
+      case RoomPreloaded(_, level, Some(club), _) =>
+        0.15
+      case RoomPreloaded(_, _, _, Some(lesson)) =>
         0.5 * (lesson.requiredAttendance - attendance.getOrElse(lesson.id, 0))
-      case RoomPreloaded(_, None, None) =>
-        0 // library?
+      case RoomPreloaded(_, _, None, None) =>
+        ??? // library?
     }
 
     nextRoom match {
@@ -38,13 +38,13 @@ object Travel {
       case Some(room) =>
         ??? // Library
       case None =>
-        val classesByLevel = rooms.filter(_.lesson.isDefined).sortBy(c => (c.lesson.get.academicYear, c.number))
-
-        val maxRoomLevel = classesByLevel.head.lesson.get.academicYear
-        if (maxRoomLevel <= student.academicYear)
-          ContinueTravelling(classesByLevel.head.number) // travel further (higher room numbers => higher academic year)
-        else
-          ContinueTravelling(classesByLevel.last.number) // travel back (lower room numbers => lower academic year)
+        val roomsByLevel = rooms.sortBy(c => (c.level, c.number))
+        ContinueTravelling(
+          if (roomsByLevel.head.level <= student.academicYear)
+            roomsByLevel.head.number // travel further (higher room numbers => higher academic year)
+          else
+            roomsByLevel.last.number // travel back (lower room numbers => lower academic year)
+        )
     }
   }
 }
