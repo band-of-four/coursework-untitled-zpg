db9d7a376bd8b23a6ac8571ca396c68f190665d7
johnny-keker <giomail.iv@gmail.com>
2019-01-11 17:19:30 +0300

    character creation form

--- client/src/Intro.vue
+++ client/src/Intro.vue
@@ -3,17 +3,31 @@
   <span>
     Смотрю, ты нашел новую душу в нашу школу, Хранитель? Представь же ее мне.
   </span>
+  <form @submit.prevent="create">
+    <input type="text" v-model="name" placeholder="Имя персонажа">
+    <br>
+    <input type="radio" id="female" value="Female" v-model="gender">
+    <label for="female">Женщина</label>
+    <br>
+    <input type="radio" id="male" value="Male" v-model="gender">
+    <label for="male">Мужщина</label>
+    <br>
+  </form>
   <button @click="create">Продолжить</button>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'Auth',
+  name: 'Intro',
+  data: () => ({
+    name: '',
+    gender: 'Female'
+  }),
   components: {},
   methods: {
     async create() {
-      await this.$store.dispatch('createStudent', { name: 'Lain', gender: 'Female' });
+      await this.$store.dispatch('createStudent', { name: this.name, gender: this.gender });
       this.$router.replace('/');
     }
   }
