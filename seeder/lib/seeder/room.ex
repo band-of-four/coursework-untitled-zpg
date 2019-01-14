4a4469bbb7e39d9832f59b25d1211ebad335483a
johnny-keker <giomail.iv@gmail.com>
2019-01-14 04:33:29 +0300

    rooms seeds including libraries and infirmaries

--- seeder/lib/seeder/room.ex
+++ seeder/lib/seeder/room.ex
@@ -19,6 +19,12 @@ defmodule Seeder.Room do
         [%Room{kind: "Classroom", level: k, lesson: l} | acc]
       end)
     end)
+    |> Enum.map(fn arr -> 
+      List.insert_at(arr, Enum.random(0..length(arr)), %Room{kind: "Library", level: List.first(arr).level})
+    end)
+    |> Enum.map(fn arr -> 
+      List.insert_at(arr, Enum.random(0..length(arr)), %Room{kind: "Infirmary", level: List.first(arr).level})
+    end)
     |> Enum.map(fn arr ->
       {List.first(arr).level, arr}
     end)
