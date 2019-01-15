8ca378033a8617a41ec6f02a01d282aec6af6978
timlathy <tim.labushev@gmail.com>
2019-01-15 18:26:01 +0300

    student relationships

--- server/conf/evolutions/default/8.sql
+++ server/conf/evolutions/default/8.sql
@@ -53,20 +53,49 @@ create function lesson_attendance_update_at_lesson_end(in in_student_id bigint)
   language plpgsql;;
 
 create table student_letters (
+  id bigserial primary key,
   sender_id bigint not null references students,
-  receiver_id bigint not null references students,
-  club_id bigint not null references student_clubs
+  receiver_id bigint not null references students
 );;
 
-create table relationships (
+create table student_relationships (
   student_a bigint not null references students,
   student_b bigint not null references students,
-  relationship integer
+  relationship integer not null,
+  delta integer not null
 );;
+
+create function student_relationships_update_in_club(in in_student_id bigint)
+  returns void as $$
+    begin
+      with letters as (
+        select distinct on (sl.sender_id) sl.id, sl.sender_id, sp.power
+        from student_letters sl
+        inner join spells_students spst on spst.student_id = sl.sender_id
+        inner join spells sp on sp.id = spst.spell_id and sp.kind = 'Charisma'
+        where sl.receiver_id = in_student_id
+      )
+      insert into student_relationships as sr (student_a, student_b, relationship)
+        (select l.sender_id, in_student_id, l.power from letters l where l.sender_id < ?)
+        union
+        (select in_student_id, l.sender_id, l.power from letters l where l.sender_id > ?)
+      on conflict (student_a, student_b)
+         do update set relationship = sr.relationship + (
+           case sr.relationship
+             when 0 then
+               (select l.power from letters l where l.sender_id = sr.student_a or l.sender_id = sr.student_b)
+             else
+               ((select l.power from letters l where l.sender_id = sr.student_a or l.sender_id = sr.student_b) / sr.relationship)
+             end
+           );;
+    end;;
+  $$
+  language plpgsql;;
 
 # --- !Downs
 
-drop table relationships;;
+drop function student_relationships_update_in_club(bigint);;
+drop table student_relationships;;
 drop table student_letters;;
 drop function lesson_attendance_update_at_lesson_end(bigint);;
 drop table lesson_attendances;;
