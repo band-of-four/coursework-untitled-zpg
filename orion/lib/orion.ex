aa10493c5f001ecc7507a455239768112d33cdfa
johnny-keker <giomail.iv@gmail.com>
2019-01-17 01:57:08 +0300

    orion born

---
+++ orion/lib/orion.ex
@@ -0,0 +1,6 @@
+defmodule Orion do
+  import Nadia
+  def run do
+    Nadia.send_message(487867125, "Hello, Father!")
+  end
+end
---
+++ orion/mix.exs
@@ -0,0 +1,29 @@
+defmodule Orion.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :orion,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      applications: [:nadia],
+      extra_applications: [:logger],
+      mod: {Orion.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:nadia, "~> 0.4.4"}
+    ]
+  end
+end
