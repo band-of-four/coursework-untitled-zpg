d43256a64582a7d38ef648a67985c1aaea5fa5a5
johnny-keker <giomail.iv@gmail.com>
2019-01-17 02:28:20 +0300

    orion added button

--- orion/lib/orion.ex
+++ orion/lib/orion.ex
@@ -1,6 +1,10 @@
 defmodule Orion do
   import Nadia
+  @chat_id 487867125
+
   def run do
-    Nadia.send_message(487867125, "Hello, Father!")
+    button = %Nadia.Model.KeyboardButton{text: "Hello"}
+    keyboard = %Nadia.Model.ReplyKeyboardMarkup{keyboard: [[button]]}
+    Nadia.send_message(@chat_id, "Hello!", [{:reply_markup, keyboard}])
   end
 end
