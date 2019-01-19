b8a3c0b327f0fa9a7c62a972e3011a0fadd6bf91
timlathy <tim.labushev@gmail.com>
2019-01-19 19:57:47 +0300

    rest conventions strike again

--- client/src/api/note.js
+++ client/src/api/note.js
@@ -3,5 +3,5 @@ import { getResource, post } from './index.js';
 export const getDiarySections = getResource('/notes/diary');
 
 export function postHeart(id) {
-  return post(`/notes/heart/${id}`, undefined).then((r) => r.json());
+  return post(`/notes/${id}/heart`, undefined).then((r) => r.json());
 }
