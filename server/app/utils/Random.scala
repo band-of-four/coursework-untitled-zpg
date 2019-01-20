2a54f1f0e952a5ebc8db26ca252b4c46daab17c7
timlathy <tim.labushev@gmail.com>
2019-01-20 11:57:43 +0300

    weighted sampler shouldn't fail when the sum of weights is 0

--- server/app/utils/Random.scala
+++ server/app/utils/Random.scala
@@ -14,14 +14,19 @@ object WeightedSample {
   def apply[A](elements: Seq[A])(weightMap: A => Double): Option[A] = {
     val weights = elements.map(weightMap)
 
-    val r = ThreadLocalRandom.current().nextDouble(0, weights.sum)
+    weights.sum match {
+      case 0.0 =>
+        None
+      case weightSum =>
+        val r = ThreadLocalRandom.current().nextDouble(0, weightSum)
 
-    var currentWeight = 0.0
-    for ((item, weight) <- elements.zip(weights)) {
-      currentWeight += weight
-      if (currentWeight >= r) return Some(item)
+        var currentWeight = 0.0
+        for ((item, weight) <- elements.zip(weights)) {
+          currentWeight += weight
+          if (currentWeight >= r) return Some(item)
+        }
+
+        None
     }
-
-    None
   }
 }
