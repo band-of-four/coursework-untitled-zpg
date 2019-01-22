326cc8fc4bf4396c8597885269990ed9670cb035
timlathy <tim.labushev@gmail.com>
2019-01-22 13:12:24 +0300

    style stage notes

--- client/src/views/Overview.vue
+++ client/src/views/Overview.vue
@@ -1,29 +1,13 @@
 <template>
 <div>
   <section class="page-section">
-    <h1>{{ name }}, {{ level }}-й год</h1>
-    <section v-if="note.stage === 'Travel'">
-      Портреты на стенах
-      <div>"{{ note.text }}"</div>
-    </section>
-    <section v-else-if="note.stage === 'Fight'">
-      Шум в коридоре
-      <div>{{ note.creature }}</div>
-      <div>"{{ note.text }}"</div>
-    </section>
-    <section v-else-if="note.stage === 'Lesson'">
-      Слева от парты распахнуто окно
-      <div>{{ note.lesson }}</div>
-      <div>"{{ note.text }}"</div>
-    </section>
-    <section v-else-if="note.stage === 'Infirmary'">
-      Прививки
-      <div>"{{ note.text }}"</div>
-    </section>
-    <section v-else-if="note.stage === 'Library'">
-      Где-то в кармане был читательский билет
-      <div>"{{ note.text }}"</div>
-    </section>
+    <h1 class="page-section__heading">{{ name }}, {{ level }}-й год</h1>
+    <div class="stage-note">
+      <div class="stage-note__heading">
+        {{ stageNote.subject }} — {{ stageNote.label }}
+      </div>
+      <div class="stage-note__text">{{ stageNote.text }}</div>
+    </div>
     <div class="progress">
       <div class="progress__neg-value" :style="{ width: `${100 - progress}%` }"></div>
     </div>
@@ -49,11 +33,23 @@ export default {
     ...mapActions({ loadSpells: 'spells/load', loadAttendance: 'attendance/load' })
   },
   computed: {
-    ...mapState('stage', ['note']),
     ...mapState(['spells', 'attendance']),
     ...mapState('student', ['name', 'level', 'hp']),
     ...mapGetters('stage', ['progress']),
-    ...mapGetters('stage', ['progress']),
+    stageNote() {
+      const note = this.$store.state.stage.note;
+
+      const label = note.stage === 'Travel' ? 'портреты на стенах'
+                  : note.stage === 'Fight' ? 'шум в коридоре!'
+                  : note.stage === 'Lesson' ? 'слева от парты распахнуто окно'
+                  : note.stage === 'Club' ? 'интересно...'
+                  : note.stage === 'Library' ? 'где-то в кармане был читательский билет'
+                  : note.stage === 'Infirmary' ? 'вижу свет' : '';
+
+      const subject = note.creature || note.lesson || note.club || 'Школа';
+
+      return { label, subject, text: note.text };
+    }
   }
 }
 </script>
