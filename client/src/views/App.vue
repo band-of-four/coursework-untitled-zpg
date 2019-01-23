538a11eb8f1467a545fc1b15a9eb3d23119984c5
timlathy <tim.labushev@gmail.com>
2019-01-24 00:49:10 +0300

    header

--- client/src/views/App.vue
+++ client/src/views/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="loading" v-if="loading"></div>
-  <div v-else :class="{ content: signedIn && student }">
-    <section v-if="signedIn && student">
-      <router-link to="/">Обзор</router-link>
-      <router-link to="/diary">Дневник</router-link>
-      <router-link to="/owls">Совы</router-link>
-      <router-link to="/relationships">Отношения</router-link>
-      <router-link to="/skills">Навыки</router-link>
-      <router-link to="/suggest">Предложить</router-link>
-      <a href="/auth/signout">Выйти</a>
-    </section>
-    <router-view></router-view>
+  <div v-else>
+    <header class="header" v-if="signedIn && student">
+      <div class="header__content">
+        <router-link class="header-logo" to="/">Sumaju nikki</router-link>
+        <router-link class="header-link" to="/diary">Дневник</router-link>
+        <router-link class="header-link" to="/owls">Совы</router-link>
+        <router-link class="header-link" to="/relationships">Отношения</router-link>
+        <router-link class="header-link" to="/skills">Навыки</router-link>
+        <router-link class="header-link" to="/suggest">Предложить</router-link>
+        <a class="header-link header-link--signout" href="/auth/signout">Выйти</a>
+      </div>
+    </header>
+    <router-view :class="{ content: signedIn && student }"></router-view>
   </div>
 </template>
 
