941fcaa1fb5d489350c6bd189203d06f6d0f5ac3
timlathy <tim.labushev@gmail.com>
2019-01-24 04:40:19 +0300

    move css around, add a burger icon (no menu yet)

--- client/src/views/App.vue
+++ client/src/views/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="loading" v-if="loading"></div>
   <div v-else>
-    <header class="header" v-if="signedIn && student">
+    <header class="header m-hide" v-if="signedIn && student">
       <div class="header__content">
         <router-link class="header-logo" to="/">Sumaju nikki</router-link>
         <router-link class="header-link" to="/diary">Дневник</router-link>
@@ -11,6 +11,10 @@
         <router-link class="header-link" to="/suggest">Предложить</router-link>
         <a class="header-link header-link--signout" href="/auth/signout">Выйти</a>
       </div>
+    </header>
+    <header class="header header--mobile m-show" v-if="signedIn && student">
+      <router-link class="header-logo" to="/">Sumaju nikki</router-link>
+      <router-link class="header-icon fa-bars-mask" to="/menu"></router-link>
     </header>
     <router-view :class="{ content: signedIn && student }"></router-view>
   </div>
