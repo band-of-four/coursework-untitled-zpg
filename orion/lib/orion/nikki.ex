0ab1f6470bc827b9bc01c45a8b4698247350d21f
johnny-keker <giomail.iv@gmail.com>
2019-01-24 20:47:24 +0300

    specs/presentation

--- orion/lib/orion/nikki.ex
+++ orion/lib/orion/nikki.ex
@@ -3,7 +3,7 @@ defmodule Orion.Nikki do
   @token "trust-me-im-orion"
 
   def get_unapproved_creature() do
-    case HTTPoison.get("localhost:9000/bot/creatures/unapproved", [{"Token", @token}]) do
+    case HTTPoison.get("localhost:9090/bot/creatures/unapproved", [{"Token", @token}]) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         Poison.Parser.parse!(body, %{keys: :atoms}) 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
@@ -15,7 +15,7 @@ defmodule Orion.Nikki do
   end
 
   def get_unapproved_note do
-    case HTTPoison.get("localhost:9000/bot/notes/unapproved", [{"Token", @token}]) do
+    case HTTPoison.get("localhost:9090/bot/notes/unapproved", [{"Token", @token}]) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         Poison.Parser.parse!(body, %{keys: :atoms}) 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
@@ -27,12 +27,12 @@ defmodule Orion.Nikki do
   end
 
   def post_approved_creature(data) do
-    HTTPoison.post "localhost:9000/bot/creatures/approve", Poison.encode!(data), 
+    HTTPoison.post "localhost:9090/bot/creatures/approve", Poison.encode!(data), 
       [{"Content-Type", "application/json"}, {"Token", @token}]
   end
 
   def post_approved_note(data) do
-    HTTPoison.post "localhost:9000/bot/notes/approve", Poison.encode!(data), 
+    HTTPoison.post "localhost:9090/bot/notes/approve", Poison.encode!(data), 
       [{"Content-Type", "application/json"}, {"Token", @token}]
   end
 end
